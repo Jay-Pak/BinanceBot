76a1e1a91320e2070c1103e28542e15d1a16fa8e
Jay-Pak <119163837+Jay-Pak@users.noreply.github.com>
2023-01-08 22:23:57 +0900

    01/08 by JAY

--- Code/BackTesting.py
+++ Code/BackTesting.py
@@ -38,9 +38,18 @@ def binance_btc_liq_balance(wallet_balance, contract_qty, entry_price):
             break
     return round(liq_price, 2)
 
+#수익금
+def amount_of_profit(user_price, closed_price, margin, leverage):
+    result = leverage * ((closed_price - user_price)/user_price) * margin
+    return abs(result)
+
+#수익률
+def profit_Rate(profit, margin):
+    result = profit/margin * 100
+    return result
 
 #일봉 백테스팅
-directory = 'Data\BTCUSDT_Binance_futures_UM_day.csv'
+directory = 'Data\BTCUSDT_Binance_futures_UM_hour.csv'
 temp_btc1d = pd.read_csv(directory)
 btc1d = temp_btc1d.loc[::-1].reset_index(drop=True)
 btc1d['Date'] = pd.to_datetime(btc1d.Date)
@@ -51,23 +60,117 @@ RSI = RSI(btc1d)
 #BTC1D Call 방법 인덱스 : BTC1D.index[i], Close: BTC1D['Close'][i], RSI: BTC1D['RSI'][i]
 BTC1D = pd.concat([btc1d['Close'], RSI], axis = 1)
 
-total_balance = 5
-betSize = total_balance/5
 leverage = 5
+margin = 1000
+total_balance = margin * leverage
+betSize = total_balance/7
 status = ''
 user_price = 0
 user_liq_price = 0
-user_RSI = 0
-user_date = 0
 count = 0
+pre_betSize = 0
+
+total_profit = 0
+win_rate = 0
+win = 0
+lose = 0
+
 
 for i in range(len(BTC1D)):
-    if(count == 0 & BTC1D['RSI'][i] <= 25):
+    if(count == 0 and BTC1D['RSI'][i] <= 25):
         status = 'long'
         user_price = BTC1D['Close'][i]
-        user_RSI = BTC1D['RSI'][i]
-        user_date = BTC1D.index[i]
         count += 1
-    elif(count != 0 & count < 5):
-        user_price = ((user_price*betSize*count) + BTC1D['Close'][i]*betSize)/(betSize*(count+1))
+        pre_betSize = betSize
+        continue
+    elif(count == 0 and BTC1D['RSI'][i] >= 75):
+        status = 'short'
+        user_price = BTC1D['Close'][i]
+        count += 1
+        pre_betSize = betSize
+        continue
+    
+    if(count >= 1 and count < 7 and status == 'long'):
+        if(BTC1D['RSI'][i] <= 30):
+            user_price = ((user_price*pre_betSize) + BTC1D['Close'][i]*(betSize))/(pre_betSize + betSize)
+            pre_betSize += betSize
+            count += 1
+            continue
+        else:
+            count += 1
+    elif(count >=1 and count < 7 and status == 'short'):
+        if(BTC1D['RSI'][i] >= 70):
+            user_price = ((user_price*pre_betSize) + BTC1D['Close'][i]*(betSize))/(pre_betSize + betSize)
+            pre_betSize += betSize
+            count += 1
+            continue
+        else:
+            count += 1
         
+    if(status == "long" and ((BTC1D['Close'][i])-user_price) > 0 and profit_Rate(amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage), pre_betSize) >= 20):
+        total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage)
+        win += 1
+        count = 0
+        status = ""
+        user_price = 0
+        pre_betSize = 0
+        continue
+            
+    elif(status == "long" and ((BTC1D['Close'][i])-user_price) < 0 and profit_Rate(amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage), pre_betSize) <= -10):
+        total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage) * -1
+        lose += 1
+        count = 0
+        status = ""
+        user_price = 0
+        pre_betSize = 0
+        continue
+        
+    elif(status == "short" and ((BTC1D['Close'][i])-user_price) < 0 and profit_Rate(amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage), pre_betSize) >= 20):
+        total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage)
+        win += 1
+        count = 0
+        status = ""
+        user_price = 0
+        pre_betSize = 0
+        continue
+        
+    elif(status == "short" and ((BTC1D['Close'][i])-user_price) > 0 and profit_Rate(amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage), pre_betSize) <= -10):
+        total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage) * -1
+        lose += 1
+        count = 0
+        status = ""
+        user_price = 0
+        pre_betSize = 0
+        continue
+    
+    if(count == 7 and status == 'long'):
+        if((BTC1D['Close'][i] - user_price) >= 0):
+            total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage)
+        else:
+            total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage) * -1
+        count = 0
+        status = ""
+        user_price = 0
+        pre_betSize = 0
+    elif(count == 7 and status == 'short'):
+        if((BTC1D['Close'][i] - user_price) < 0):
+            total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage)
+        else:
+            total_profit += amount_of_profit(user_price, BTC1D['Close'][i], margin, leverage) * -1
+        count = 0
+        status = ""
+        user_price = 0
+        pre_betSize = 0
+
+print(total_profit, win, lose)
+
+
+# #수익금
+# def amount_of_profit(user_price, closed_price, margin, leverage):
+#     result = leverage * ((closed_price - user_price)/user_price) * margin
+#     return abs(result)
+
+# #수익률
+# def profit_Rate(profit, margin):
+#     result = profit/margin * 100
+#     return result
--- Code/temp.py
+++ Code/temp.py
@@ -1,11 +1,13 @@
-total_balance = 5000
 user_price = 10000
-count=1
-price=5000
-betSize_per = total_balance/5
-betSize = betSize_per/price
-pre_betSize = betSize_per/user_price
+pre_betSize = 2000
 
-user_price = (user_price*pre_betSize + price*betSize)/(pre_betSize+betSize)
-#                                                       다음 런에서 분모가 pre_betSize가 됨
-print(user_price)
+current_price = 20000
+betSize = 1000
+
+
+
+user_price = ((user_price * pre_betSize) + (current_price * betSize)) / (pre_betSize + betSize)
+
+pre_betSize += betSize
+
+print(user_price, pre_betSize)
